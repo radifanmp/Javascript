dc137b58ea7d0a14c961cf64da4b1c62ab0648b6
Radifan Mardhi Prana <43719706+radifanmp@users.noreply.github.com>
2021-06-10 15:35:30 +0700

    Type Data

---
+++ index.js
@@ -0,0 +1,51 @@
+// Primitive Data Type
+
+//String ("",'')
+let name = "Adi";
+const student = "Fadhik";
+var merk = "BMW";
+
+//Integer / Number
+//Integer = Bilangan Genap
+let int = 2;
+
+//Float = Bilangan Desimal
+let float = 2.5;
+
+//Boolean = Tipe data pembanding, true / false (0 / 1)
+var isSleeping = false;
+var isStudying = true;
+
+var value1 = 20,
+  value2 = 30,
+  value3 = 40;
+
+//Null = Tipe data ini berfungsi untuk memberikan nilai 0
+var score = null;
+
+// var score = ""
+// var score = 0
+// var score = false
+
+//Undifined = Tidak terdefinisi
+var value;
+
+document.write(typeof name === "string");
+document.write("<br>");
+document.write(typeof int === "number");
+document.write("<br>");
+document.write(typeof float === "number");
+
+document.write("<br>");
+document.write("<br>");
+// ????
+document.write(value1 < value2);
+
+document.write("<br>");
+// ???
+document.write(value1 > value3);
+
+document.write("<br>");
+document.write("<br>");
+document.write(isSleeping + "<br>");
+document.write(isStudying + "<br>");
